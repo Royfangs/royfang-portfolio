4a399fc03737b1ad9452b456e42e15d846d2feee
Royfangs <royfangs@gmail.com>
2019-02-25 15:49:08 +0800

    Updates

---
+++ precache-manifest.1107ef1da53b682ac8e24671b49cd137.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "20bb2b728c7b6292540118fbb950195d",
+    "url": "/royfang-portfolio/static/media/expensify.20bb2b72.png"
+  },
+  {
+    "revision": "9845c86f675c4762eff5",
+    "url": "/royfang-portfolio/static/css/main.637b8ce6.chunk.css"
+  },
+  {
+    "revision": "8f041c0347f12b0fba2e",
+    "url": "/royfang-portfolio/static/js/1.8f041c03.chunk.js"
+  },
+  {
+    "revision": "ccdd8bddbfff308fc680",
+    "url": "/royfang-portfolio/static/js/runtime~main.ccdd8bdd.js"
+  },
+  {
+    "revision": "75a61a21e517a833f2ecf8c13d677d06",
+    "url": "/royfang-portfolio/static/media/portfolio.75a61a21.jpg"
+  },
+  {
+    "revision": "9845c86f675c4762eff5",
+    "url": "/royfang-portfolio/static/js/main.9845c86f.chunk.js"
+  },
+  {
+    "revision": "35fb6e1dbb98c990488f96f0f19c98b5",
+    "url": "/royfang-portfolio/static/media/smart-brain.35fb6e1d.png"
+  },
+  {
+    "revision": "89921d10db10f33a8b21b1d69f36f77a",
+    "url": "/royfang-portfolio/static/media/mtg-searche.89921d10.png"
+  },
+  {
+    "revision": "9a9f661095af54f90031cbea032bb586",
+    "url": "/royfang-portfolio/static/media/grace.9a9f6610.png"
+  },
+  {
+    "revision": "2ca8e6e283e46da9d1599811d45a27c8",
+    "url": "/royfang-portfolio/static/media/nodejs-shop-example.2ca8e6e2.png"
+  },
+  {
+    "revision": "90d3f70131143259b341a135f935ce7e",
+    "url": "/royfang-portfolio/index.html"
+  }
+];
