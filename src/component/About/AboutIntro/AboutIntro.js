8428faa73a1e1133813012afe6d11eedbeadfc98
Royfangs <royfangs@gmail.com>
2019-06-11 08:53:23 +0800

    adjust AboutIntro page.

--- src/component/About/AboutIntro/AboutIntro.js
+++ src/component/About/AboutIntro/AboutIntro.js
@@ -4,8 +4,8 @@ import './AboutIntro.scss';
 const AboutIntro = () => (
   <div>
     <p className="AboutIntro">
-      Hi, my name is Roy Fang, I'm a self-taught Front-End Developer from Taiwan.
-      Having serious passion on web developing. I love to dig Front-End related knowledge and coding, and want to be a Full-Stack web Developer.
+      Hi, my name is Roy Fang, a Front-End Developer from Taiwan.
+      Having serious passion on web developing. I love to dig Front-End related knowledge and coding, and enjoy solving problems.
     </p>
   </div>
 );
